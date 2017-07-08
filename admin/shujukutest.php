43ae57da95470395b520e2b5e08ed8cf9789d5c7
JiangJun123 <1146583283@qq.com>
2017-07-08 15:56:55 +0800

    Init commit

--- admin/shujukutest.php
+++ admin/shujukutest.php
@@ -1,26 +1,13 @@
 <?php
 $servername = "localhost";
 $username = "jj";
-$password = "123";
-$dbname = "xcyl";
- 
+$password = "";
+
 // 创建连接
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+$conn = new mysqli($servername, $username, $password);
+// 检测连接
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
-} 
- 
-$sql = "SELECT user, bankuai FROM MyGuests";
-$result = $conn->query($sql);
- 
-if ($result->num_rows > 0) {
-    // 输出数据
-    while($row = $result->fetch_assoc()) {
-        echo "bankuai: " . $row["bankuai"]. " - Name: " . $row["user"].   "<br>";
-    }
-} else {
-    echo "0 结果";
 }
-$conn->close();
+echo "连接成功";
 ?>
